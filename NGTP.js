befaeef589ed0304c2dd0ac8f97ee0f5a1cedbca
lakshit0011 <gehlotlakshit0@gmail.com>
2024-10-03 19:24:05 +0530

    chamge all

--- NGTP.js
+++ NGTP.js
@@ -1,4 +1,21 @@
-function toggleMenu() {
-  const menu = document.querySelector(".navbar ul");
-  menu.classList.toggle("active");
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+const totalSlides = slides.length;
+
+function updateSliderPosition() {
+  const gallerySlider = document.querySelector(".gallery-slider");
+  const slideWidth = slides[0].clientWidth;
+  gallerySlider.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
 }
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % totalSlides;
+  updateSliderPosition();
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+  updateSliderPosition();
+}
+
+window.addEventListener("resize", updateSliderPosition);
